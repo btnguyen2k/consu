2d216c3ac5d5702cc2250ffebdc60e74f3889661
Thanh Nguyen <btnguyen2k@gmail.com>
2019-01-12 04:24:12 +0700

    Update godoc

--- semita/Semita.go
+++ semita/Semita.go
@@ -1,1 +1,7 @@
+// Package semita provides utility functions to access data from a hierarchy structure.
+//
+// Notation:
+//   - . (the dot character): path separator
+//   - name: access a map's attribute specified by name
+//   [i]: access i'th element of a list/array (0-based)
 package semita
