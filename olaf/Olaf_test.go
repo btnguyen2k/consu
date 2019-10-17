cf480af9dfdfceee921200b9f3447d57eccb9e3d
Thanh Nguyen <btnguyen2k@gmail.com>
2019-10-17 21:36:47 +0700

    More tests

--- olaf/Olaf_test.go
+++ olaf/Olaf_test.go
@@ -2,6 +2,7 @@ package olaf
 
 import (
 	"math/big"
+	"sync"
 	"testing"
 	"time"
 )
@@ -11,7 +12,7 @@ func TestUnixMilliseconds(t *testing.T) {
 	now := time.Now()
 	delta := now.UnixNano() - ms*1000000
 	if delta < 0 || delta >= 1000000 {
-		t.Errorf("TestUnixMilliseconds failed, ms: %d, now: %d, delta: %d.", ms, now.UnixNano(), delta)
+		t.Fatalf("TestUnixMilliseconds failed, ms: %d, now: %d, delta: %d.", ms, now.UnixNano(), delta)
 	}
 }
 
@@ -23,10 +24,10 @@ func TestWaitTillNextMillisec(t *testing.T) {
 	endMs := end.UnixNano() / 1000000
 	delta := nextMs - startMs
 	if 0 >= delta {
-		t.Errorf("Next milliseconds was incorrect, prevMs: %d, nextMs: %d, delta: %d", startMs, nextMs, delta)
+		t.Fatalf("Next milliseconds was incorrect, prevMs: %d, nextMs: %d, delta: %d", startMs, nextMs, delta)
 	}
 	if endMs < nextMs {
-		t.Errorf("Next milliseconds must not greater than now, nextMs: %d, nowMs: %d.", nextMs, endMs)
+		t.Fatalf("Next milliseconds must not greater than now, nextMs: %d, nowMs: %d.", nextMs, endMs)
 	}
 }
 
@@ -34,10 +35,10 @@ func TestNewOlaf(t *testing.T) {
 	nodeId := int64(1981)
 	o := NewOlaf(nodeId)
 	if o.NodeID != nodeId {
-		t.Errorf("Invalid Olaf instance, expected NodeId: %d, actual NodeId: %d.", nodeId, o.NodeID)
+		t.Fatalf("Invalid Olaf instance, expected NodeId: %d, actual NodeId: %d.", nodeId, o.NodeID)
 	}
 	if o.Epoch != Epoch {
-		t.Errorf("Invalid Olaf instance, expected Epoch: %d, actual Epoch: %d.", Epoch, o.Epoch)
+		t.Fatalf("Invalid Olaf instance, expected Epoch: %d, actual Epoch: %d.", Epoch, o.Epoch)
 	}
 }
 
@@ -46,10 +47,10 @@ func TestNewOlafWithEpoch(t *testing.T) {
 	epoch := int64(123456789)
 	o := NewOlafWithEpoch(nodeId, epoch)
 	if o.NodeID != nodeId {
-		t.Errorf("Invalid Olaf instance, expected NodeId: %d, actual NodeId: %d.", nodeId, o.NodeID)
+		t.Fatalf("Invalid Olaf instance, expected NodeId: %d, actual NodeId: %d.", nodeId, o.NodeID)
 	}
 	if o.Epoch != epoch {
-		t.Errorf("Invalid Olaf instance, expected Epoch: %d, actual Epoch: %d.", epoch, o.Epoch)
+		t.Fatalf("Invalid Olaf instance, expected Epoch: %d, actual Epoch: %d.", epoch, o.Epoch)
 	}
 }
 
@@ -61,7 +62,34 @@ func TestOlaf_Id64(t *testing.T) {
 		id := o.Id64()
 		items[i] = id
 		if i > 0 && items[i] <= items[i-1] {
-			t.Errorf("Generated ID is invalid: items[%d]=%d must be less than items[%d]=%d.", i-1, items[i-1], i, items[i])
+			t.Fatalf("Generated ID is invalid: items[%d]=%d must be less than items[%d]=%d.", i-1, items[i-1], i, items[i])
+		}
+	}
+}
+
+func TestOlaf_Id64MultiThreads(t *testing.T) {
+	const numThreads = 4
+	const numItemsPerThread = 10000000 / numThreads
+	const numItems = numItemsPerThread * numThreads
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
+	items := [numItems]uint64{}
+	o := NewOlaf(1981)
+	for i := 0; i < numThreads; i++ {
+		go func(threadIndex int, wg *sync.WaitGroup) {
+			startIndex := threadIndex * numItemsPerThread
+			for i := 0; i < numItemsPerThread; i++ {
+				items[startIndex+i] = o.Id64()
+			}
+			wg.Done()
+		}(i, &wg)
+	}
+	wg.Wait()
+	itemsMap := make(map[uint64]bool)
+	for _, v := range items {
+		_, ok := itemsMap[v]
+		if v == 0 || ok {
+			t.Fatalf("Generated ID is invalid: items[%v]=%v", v, v)
 		}
 	}
 }
@@ -74,7 +102,7 @@ func TestOlaf_Id64Hex(t *testing.T) {
 		id := o.Id64Hex()
 		items[i] = id
 		if i > 0 && items[i] <= items[i-1] {
-			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+			t.Fatalf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
 		}
 	}
 }
@@ -87,7 +115,7 @@ func TestOlaf_Id64Ascii(t *testing.T) {
 		id := o.Id64Ascii()
 		items[i] = id
 		if i > 0 && items[i] <= items[i-1] {
-			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+			t.Fatalf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
 		}
 	}
 }
@@ -100,7 +128,35 @@ func TestOlaf_Id128(t *testing.T) {
 		id := o.Id128()
 		items[i] = id
 		if i > 0 && items[i].Cmp(items[i-1]) <= 0 {
-			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1].String(), i, items[i].String())
+			t.Fatalf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1].String(), i, items[i].String())
+		}
+	}
+}
+
+func TestOlaf_Id128MultiThreads(t *testing.T) {
+	const numThreads = 4
+	const numItemsPerThread = 10000000 / numThreads
+	const numItems = numItemsPerThread * numThreads
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
+	items := [numItems]*big.Int{}
+	o := NewOlaf(1981)
+	for i := 0; i < numThreads; i++ {
+		go func(threadIndex int, wg *sync.WaitGroup) {
+			startIndex := threadIndex * numItemsPerThread
+			for i := 0; i < numItemsPerThread; i++ {
+				items[startIndex+i] = o.Id128()
+			}
+			wg.Done()
+		}(i, &wg)
+	}
+	wg.Wait()
+	itemsMap := make(map[string]bool)
+	for _, v := range items {
+		s := v.String()
+		_, ok := itemsMap[s]
+		if v.Cmp(big.NewInt(0)) == 0 || ok {
+			t.Fatalf("Generated ID is invalid: items[%s]=%v", s, v)
 		}
 	}
 }
@@ -113,7 +169,7 @@ func TestOlaf_Id128Hex(t *testing.T) {
 		id := o.Id128Hex()
 		items[i] = id
 		if i > 0 && items[i] <= items[i-1] {
-			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+			t.Fatalf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
 		}
 	}
 }
@@ -126,7 +182,7 @@ func TestOlaf_Id128Ascii(t *testing.T) {
 		id := o.Id128Ascii()
 		items[i] = id
 		if i > 0 && items[i] <= items[i-1] {
-			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+			t.Fatalf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
 		}
 	}
 }
@@ -139,7 +195,7 @@ func TestOlaf_ExtractTime64(t *testing.T) {
 	delta := time.UnixNano() - now.UnixNano()
 	if delta > 1000000 || delta < -1000000 {
 		// delta must be within 1ms
-		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+		t.Fatalf("Invalid extracted time %s (delta nano: %d).", time, delta)
 	}
 }
 
@@ -151,7 +207,7 @@ func TestOlaf_ExtractTime64Hex(t *testing.T) {
 	delta := time.UnixNano() - now.UnixNano()
 	if delta > 1000000 || delta < -1000000 {
 		// delta must be within 1ms
-		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+		t.Fatalf("Invalid extracted time %s (delta nano: %d).", time, delta)
 	}
 }
 
@@ -163,7 +219,7 @@ func TestOlaf_ExtractTime64Ascii(t *testing.T) {
 	delta := time.UnixNano() - now.UnixNano()
 	if delta > 1000000 || delta < -1000000 {
 		// delta must be within 1ms
-		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+		t.Fatalf("Invalid extracted time %s (delta nano: %d).", time, delta)
 	}
 }
 
@@ -175,7 +231,7 @@ func TestOlaf_ExtractTime128(t *testing.T) {
 	delta := time.UnixNano() - now.UnixNano()
 	if delta > 1000000 || delta < -1000000 {
 		// delta must be within 1ms
-		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+		t.Fatalf("Invalid extracted time %s (delta nano: %d).", time, delta)
 	}
 }
 
@@ -187,7 +243,7 @@ func TestOlaf_ExtractTime128Hex(t *testing.T) {
 	delta := time.UnixNano() - now.UnixNano()
 	if delta > 1000000 || delta < -1000000 {
 		// delta must be within 1ms
-		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+		t.Fatalf("Invalid extracted time %s (delta nano: %d).", time, delta)
 	}
 }
 
@@ -199,6 +255,6 @@ func TestOlaf_ExtractTime128Ascii(t *testing.T) {
 	delta := time.UnixNano() - now.UnixNano()
 	if delta > 1000000 || delta < -1000000 {
 		// delta must be within 1ms
-		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+		t.Fatalf("Invalid extracted time %s (delta nano: %d).", time, delta)
 	}
 }
